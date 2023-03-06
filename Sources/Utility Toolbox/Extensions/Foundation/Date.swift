a8686b3b57e289144116258d2c04b84ea2cb0d67
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-03-06 14:09:01 +0100

    Adds

--- Sources/Utility Toolbox/Extensions/Foundation/Date.swift
+++ Sources/Utility Toolbox/Extensions/Foundation/Date.swift
@@ -8,8 +8,65 @@
 import Foundation
 
 public extension Date {
+
+    /// Returns true if date is today, false otherwise.
     var isToday: Bool {
         let calendar = Calendar.current
         return calendar.isDateInToday(self)
+    }
+
+    /// Returns true if date is yesterday, false otherwise.
+    var isYesterday: Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInYesterday(self)
+    }
+
+    /// Returns true if date is tomorrow, false otherwise.
+    var isTomorrow: Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInTomorrow(self)
+    }
+
+    private func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+
+    private func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
+    }
+
+    /// Add components on date.
+    mutating func add(component: Calendar.Component, value: Int) {
+        let calendar = Calendar.current
+        self = calendar.date(byAdding: component, value: value, to: self) ?? Date()
+    }
+
+    /// Returns a date added with components.
+    func addingDate(component: Calendar.Component, value: Int) -> Date {
+        let calendar = Calendar.current
+        return calendar.date(byAdding: component, value: value, to: self) ?? Date()
+    }
+
+    /// Get components from date.
+    var components: DateComponents {
+        var dateComponents = DateComponents()
+
+        let date = get(.day, .month, .year, .hour, .minute, .second)
+
+        if let day = date.day,
+           let month = date.month,
+           let year = date.year,
+           let hour = date.hour,
+           let minute = date.minute,
+           let second = date.second {
+            dateComponents.day = day
+            dateComponents.month = month
+            dateComponents.year = year
+            dateComponents.hour = hour
+            dateComponents.minute = minute
+            dateComponents.second = second
+        }
+
+        return dateComponents
     }
 }
---
+++ Sources/Utility Toolbox/Extensions/SwiftUI/Image.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yann Christophe MAERTENS on 06/03/2023.
+//
+
+import SwiftUI
+
+extension Image {
+
+    /// Zoomed center of an image
+    func centerCropped(radius: CGFloat) -> some View {
+        GeometryReader { geo in
+            self
+                .resizable()
+                .scaledToFill()
+                .frame(width: geo.size.width, height: geo.size.height)
+                .clipShape(RoundedRectangle(cornerRadius: radius))
+        }
+    }
+
+    /// Get Image from data.
+    static func data(_ data: Data) -> Image? {
+        if let uiimage = UIImage(data: data) {
+            return Image(uiImage: uiimage)
+        }
+        return nil
+    }
+}
