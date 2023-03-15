357716479a220aaa39b65983bfe0cc74f71a40e3
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-03-15 14:51:59 +0100

    methods on public

--- Sources/Utility Toolbox/Models/Foundation/Managers/SaveManager.swift
+++ Sources/Utility Toolbox/Models/Foundation/Managers/SaveManager.swift
@@ -7,20 +7,20 @@
 
 import CoreData
 
-class SaveManager {
-
-    init(container: NSPersistentContainer) {
+public final class SaveManager {
+    
+    public init(container: NSPersistentContainer) {
         self.container = container
     }
-
-    enum SaveError: Error {
+    
+    private enum SaveError: Error {
         case unableToFetch
         case unableToSave
     }
-
-    var container: NSPersistentContainer
-
-    func fetchedObjects<R: NSFetchRequestResult>(entityName: String) throws -> [R] {
+    
+    public var container: NSPersistentContainer
+    
+    public func fetchedObjects<R: NSFetchRequestResult>(entityName: String) throws -> [R] {
         do {
             let request = NSFetchRequest<R>(entityName: entityName)
             return try container.viewContext.fetch(request)
@@ -28,16 +28,16 @@ class SaveManager {
             throw SaveError.unableToFetch
         }
     }
-
-    func save() throws {
+    
+    public func save() throws {
         do {
             try container.viewContext.save()
         } catch {
             throw SaveError.unableToSave
         }
     }
-
-    func delete(_ object: NSManagedObject) {
+    
+    public func delete(_ object: NSManagedObject) {
         container.viewContext.delete(object)
     }
 }
