aebbb87b8d23e36149c2f637ce31c98d5dad2f61
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-04-16 19:39:57 +0200

    Changed the next method from CaseIterable and added previous method.

--- Sources/Utility Toolbox/Extensions/Protocols/CaseIterable.swift
+++ Sources/Utility Toolbox/Extensions/Protocols/CaseIterable.swift
@@ -7,14 +7,19 @@
 
 import Foundation
 
-public extension CaseIterable where Self: Equatable {
-    /// Set to the next case.
-    mutating func next() {
-        let allCases = Self.allCases
-        // just a sanity check, as the possibility of a enum case to not be
-        // present in `allCases` is quite low
-        guard let selfIndex = allCases.firstIndex(of: self) else { return }
-        let nextIndex = Self.allCases.index(after: selfIndex)
-        self = allCases[nextIndex == allCases.endIndex ? allCases.startIndex : nextIndex]
+public extension CaseIterable where Self: Equatable, AllCases: BidirectionalCollection {
+    
+    func previous() -> Self {
+        let all = Self.allCases
+        let index = all.firstIndex(of: self)!
+        let previous = all.index(before: index)
+        return all[previous < all.startIndex ? all.index(before: all.endIndex) : previous]
+    }
+
+    func next() -> Self {
+        let all = Self.allCases
+        let index = all.firstIndex(of: self)!
+        let next = all.index(after: index)
+        return all[next == all.endIndex ? all.startIndex : next]
     }
 }
