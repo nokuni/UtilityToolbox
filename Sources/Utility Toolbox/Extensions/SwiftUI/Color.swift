05839e3353386c0b31ac5b09dfc1ead05c98e5ef
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-03 19:04:32 +0200

    Added default textfield modifier

--- Sources/Utility Toolbox/Extensions/SwiftUI/Color.swift
+++ Sources/Utility Toolbox/Extensions/SwiftUI/Color.swift
@@ -16,4 +16,22 @@ extension Color {
         let blue = Double(hex & 0x0000FF) / 255
         self.init(red: red, green: green, blue: blue, opacity: alpha)
     }
+    
+    /// A sixth-level shade of gray that adapts to the environment.
+    static let gray6 = Color(UIColor.systemGray6)
+    
+    /// A fifth-level shade of gray that adapts to the environment.
+    static let gray5 = Color(UIColor.systemGray5)
+    
+    /// A fourth-level shade of gray that adapts to the environment.
+    static let gray4 = Color(UIColor.systemGray4)
+    
+    /// A third-level shade of gray that adapts to the environment.
+    static let gray3 = Color(UIColor.systemGray3)
+    
+    /// A second-level shade of gray that adapts to the environment.
+    static let gray2 = Color(UIColor.systemGray2)
+    
+    /// The color for the main background of your interface.
+    static let background = Color(UIColor.systemBackground)
 }
