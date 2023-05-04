460df03c770ea81dace2e296e92a62d945418769
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-04 09:59:21 +0200

    Added leftover comments on SFGaming Symbol

--- Sources/Utility Toolbox/Models/Foundation/Miscellaneous/SF Symbols/SFGamingSymbol.swift
+++ Sources/Utility Toolbox/Models/Foundation/Miscellaneous/SF Symbols/SFGamingSymbol.swift
@@ -12,130 +12,255 @@ public enum SFGamingSymbol: String, CaseIterable, SFSymbolProtocol {
     case circleSquare = "circle.square"
     /// 􀧻
     case circleSquareFill = "circle.square.fill"
+    /// 􀏧
     case rectangleOnRectangle = "rectangle.on.rectangle"
+    /// 􀏨
     case rectangleFillOnRectangleFill = "rectangle.fill.on.rectangle.fill"
+    /// 􀤽
     case rectangleOnRectangleCircle = "rectangle.on.rectangle.circle"
+    /// 􀤾
     case rectangleOnRectangleCircleFill = "rectangle.on.rectangle.circle.fill"
+    /// 􀽏
     case rectangleOnRectangleSquare = "rectangle.on.rectangle.square"
+    /// 􀽐
     case rectangleOnRectangleSquareFill = "rectangle.on.rectangle.square.fill"
+    /// 􁙌
     case flagCheckered = "flag.checkered"
+    /// 􁝼
     case flagCheckeredCircle = "flag.checkered.circle"
+    /// 􁝽
     case flagCheckeredCircleFill = "flag.checkered.circle.fill"
+    /// 􀶶
     case flag2Crossed = "flag.2.crossed"
+    /// 􀶷
     case flag2CrossedFill = "flag.2.crossed.fill"
+    /// 􁜅
     case flag2CrossedCircle = "flag.2.crossed.circle"
+    /// 􁜆
     case flag2CrossedCircleFill = "flag.2.crossed.circle.fill"
+    /// 􀶸
     case flagFilledAndFlagCrossed = "flag.filled.and.flag.crossed"
+    /// 􁁜
     case flagAndFlagFilledCrossed = "flag.and.flag.filled.crossed"
+    /// 􁜔
     case flagCheckeredTwoCrossed = "flag.checkered.2.crossed"
+    /// 􀎞
     case house = "house"
+    /// 􀎟
     case houseFill = "house.fill"
+    /// 􀥆
     case houseCircle = "house.circle"
+    /// 􀥇
     case houseCircleFill = "house.circle.fill"
+    /// 􀛸
     case gamecontroller = "gamecontroller"
+    /// 􀛹
     case gamecontrollerFill = "gamecontroller.fill"
+    /// 􀦒
     case lJoystick = "l.joystick"
+    /// 􀫁
     case lJoystickFill = "l.joystick.fill"
+    /// 􀦓
     case rJoystick = "r.joystick"
+    /// 􀫂
     case rJoystickFill = "r.joystick.fill"
+    /// 􀦔
     case lJoystickPressDown = "l.joystick.press.down"
+    /// 􀫃
     case lJoystickPressDownFill = "l.joystick.press.down.fill"
+    /// 􀦕
     case rJoystickPressDown = "r.joystick.press.down"
+    /// 􀫄
     case rJoystickPressDownFill = "r.joystick.press.down.fill"
+    /// 􀿖
     case lJoystickTiltLeft = "l.joystick.tilt.left"
+    /// 􀿗
     case lJoystickTiltLeftFill = "l.joystick.tilt.left.fill"
+    /// 􀿘
     case lJoystickTiltRight = "l.joystick.tilt.right"
+    /// 􀿙
     case lJoystickTiltRightFill = "l.joystick.tilt.right.fill"
+    /// 􀿚
     case lJoystickTiltUp = "l.joystick.tilt.up"
+    /// 􀿣
     case lJoystickTiltUpFill = "l.joystick.tilt.up.fill"
+    /// 􀿤
     case lJoystickTiltDown = "l.joystick.tilt.down"
+    /// 􀿥
     case lJoystickTiltDownFill = "l.joystick.tilt.down.fill"
+    /// 􀿞
     case rJoystickTiltLeft = "r.joystick.tilt.left"
+    /// 􀿟
     case rJoystickTiltLeftFill = "r.joystick.tilt.left.fill"
+    /// 􀿠
     case rJoystickTiltRight = "r.joystick.tilt.right"
+    /// 􀿡
     case rJoystickTiltRightFill = "r.joystick.tilt.right.fill"
+    /// 􀿢
     case rJoystickTiltUp = "r.joystick.tilt.up"
+    /// 􀿣
     case rJoystickTiltUpFill = "r.joystick.tilt.up.fill"
+    /// 􀿤
     case rJoystickTiltDown = "r.joystick.tilt.down"
+    /// 􀿥
     case rJoystickTiltDownFill = "r.joystick.tilt.down.fill"
+    /// 􀧸
     case circleGridCross = "circle.grid.cross"
+    /// 􀧹
     case circleGridCrossFill = "circle.grid.cross.fill"
+    /// 􀩄
     case circleGridCrossLeftFilled = "circle.grid.cross.left.filled"
+    /// 􀩅
     case circleGridCrossUpFilled = "circle.grid.cross.up.filled"
+    /// 􀩆
     case circleGridCrossRightFilled = "circle.grid.cross.right.filled"
+    /// 􀩇
     case circleGridCrossDownFilled = "circle.grid.cross.down.filled"
+    /// 􀨲
     case dpad = "dpad"
+    /// 􀧼
     case dpadFill = "dpad.fill"
+    /// 􀧽
     case dpadLeftFilled = "dpad.left.filled"
+    /// 􀧾
     case dpadUpFilled = "dpad.up.filled"
+    /// 􀧿
     case dpadRightFilled = "dpad.right.filled"
+    /// 􀨀
     case dpadDownFilled = "dpad.down.filled"
+    /// 􀨁
     case circleCircle = "circle.circle"
+    /// 􀨂
     case circleCircleFill = "circle.circle.fill"
+    /// 􀨃
     case squareCircle = "square.circle"
+    /// 􀨄
     case squareCircleFill = "square.circle.fill"
+    /// 􀨅
     case triangleCircle = "triangle.circle"
+    /// 􀨆
     case triangleCircleFill = "triangle.circle.fill"
+    /// 􀩸
     case rectangleRoundedtop = "rectangle.roundedtop"
+    /// 􀩹
     case rectangleRoundedtopFill = "rectangle.roundedtop.fill"
+    /// 􀩺
     case rectangleRoundedbottom = "rectangle.roundedbottom"
+    /// 􀩻
     case rectangleRoundedbottomFill = "rectangle.roundedbottom.fill"
+    /// 􀨇
     case lRectangleRoundedbottom = "l.rectangle.roundedbottom"
+    /// 􀨈
     case lRectangleRoundedbottomFill = "l.rectangle.roundedbottom.fill"
+    /// 􀨉
     case l1RectangleRoundedbottom = "l1.rectangle.roundedbottom"
+    /// 􀨊
     case l1RectangleRoundedbottomFill = "l1.rectangle.roundedbottom.fill"
+    /// 􀨋
     case l2RectangleRoundedtop = "l2.rectangle.roundedtop"
+    /// 􀨌
     case l2RectangleRoundedtopFill = "l2.rectangle.roundedtop.fill"
+    /// 􀨍
     case rRectangleRoundedbottom = "r.rectangle.roundedbottom"
+    /// 􀨎
     case rRectangleRoundedbottomFill = "r.rectangle.roundedbottom.fill"
+    /// 􀨏
     case r1RectangleRoundedbottom = "r1.rectangle.roundedbottom"
+    /// 􀨐
     case r1RectangleRoundedbottomFill = "r1.rectangle.roundedbottom.fill"
+    /// 􀨑
     case r2RectangleRoundedtop = "r2.rectangle.roundedtop"
+    /// 􀨒
     case r2RectangleRoundedtopFill = "r2.rectangle.roundedtop.fill"
+    /// 􀨓
     case lbRectangleRoundedbottom = "lb.rectangle.roundedbottom"
+    /// 􀨔
     case lbRectangleRoundedbottomFill = "lb.rectangle.roundedbottom.fill"
+    /// 􀨕
     case rbRectangleRoundedbottom = "rb.rectangle.roundedbottom"
+    /// 􀨖
     case rbRectangleRoundedbottomFill = "rb.rectangle.roundedbottom.fill"
+    /// 􀨗
     case ltRectangleRoundedtop = "lt.rectangle.roundedtop"
+    /// 􀨘
     case ltRectangleRoundedtopFill = "lt.rectangle.roundedtop.fill"
+    /// 􀨙
     case rtRectangleRoundedtop = "rt.rectangle.roundedtop"
+    /// 􀨚
     case rtRectangleRoundedtopFill = "rt.rectangle.roundedtop.fill"
+    /// 􀨛
     case zlRectangleRoundedtop = "zl.rectangle.roundedtop"
+    /// 􀨜
     case zlRectangleRoundedtopFill = "zl.rectangle.roundedtop.fill"
+    /// 􀨝
     case zrRectangleRoundedtop = "zr.rectangle.roundedtop"
+    /// 􀨞
     case zrRectangleRoundedtopFill = "zr.rectangle.roundedtop.fill"
+    /// 􀾈
     case playstationLogo = "playstation.logo"
+    /// 􀾉
     case xboxLogo = "xbox.logo"
+    /// 􀧱
     case line3HorizontalCircle = "line.3.horizontal.circle"
+    /// 􀧲
     case line3HorizontalCircleFill = "line.3.horizontal.circle.fill"
+    /// 􀅼
     case plus = "plus"
+    /// 􀁌
     case plusCircle = "plus.circle"
+    /// 􀁍
     case plusCircleFill = "plus.circle.fill"
+    /// 􀅽
     case minus = "minus"
+    /// 􀁎
     case minusCircle = "minus.circle"
+    /// 􀁏
     case minusCircleFill = "minus.circle.fill"
+    /// 􀆄
     case xmark = "xmark"
+    /// 􀁠
     case xmarkCircle = "xmark.circle"
+    /// 􀁡
     case xmarkCircleFill = "xmark.circle.fill"
+    /// 􀁪
     case arrowtriangleLeftCircle = "arrowtriangle.left.circle"
+    /// 􀁫
     case arrowtriangleLeftCircleFill = "arrowtriangle.left.circle.fill"
+    /// 􀁬
     case arrowtriangleRightCircle = "arrowtriangle.right.circle"
+    /// 􀁭
     case arrowtriangleRightCircleFill = "arrowtriangle.right.circle.fill"
+    /// 􀁦
     case arrowtriangleUpCircle = "arrowtriangle.up.circle"
+    /// 􀁧
     case arrowtriangleUpCircleFill = "arrowtriangle.up.circle.fill"
+    /// 􀁨
     case arrowtriangleDownCircle = "arrowtriangle.down.circle"
+    /// 􀁩
     case arrowtriangleDownCircleFill = "arrowtriangle.down.circle.fill"
+    /// 􀀄
     case aCircle = "a.circle"
+    /// 􀀅
     case aCircleFill = "a.circle.fill"
+    /// 􀀆
     case bCircle = "b.circle"
+    /// 􀀇
     case bCircleFill = "b.circle.fill"
+    /// 􀀚
     case lCircle = "l.circle"
+    /// 􀀛
     case lCircleFill = "l.circle.fill"
+    /// 􀀦
     case rCircle = "r.circle"
+    /// 􀀧
     case rCircleFill = "r.circle.fill"
+    /// 􀀲
     case xCircle = "x.circle"
+    /// 􀀳
     case xCircleFill = "x.circle.fill"
+    /// 􀀴
     case yCircle = "y.circle"
+    /// 􀀵
     case yCircleFill = "y.circle.fill"
 
     var symbol: String {
