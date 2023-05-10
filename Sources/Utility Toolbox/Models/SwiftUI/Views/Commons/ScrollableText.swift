029a7e50a23be219b19cd39e7e96eb2024ad4ab0
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-10 15:45:05 +0200

    public again

--- Sources/Utility Toolbox/Models/SwiftUI/Views/Commons/ScrollableText.swift
+++ Sources/Utility Toolbox/Models/SwiftUI/Views/Commons/ScrollableText.swift
@@ -10,6 +10,11 @@ import SwiftUI
 public struct ScrollableText: View {
     @State private var scrollViewContentSize: CGSize = .zero
     public var text: String
+
+    public init(scrollViewContentSize: CGSize, text: String) {
+        self.text = text
+    }
+
     public var body: some View {
         ScrollView {
             Text(text)
