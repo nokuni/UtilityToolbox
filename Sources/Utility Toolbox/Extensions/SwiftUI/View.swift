38cacaef3ff779766b86553b6037430d1f1ee323
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-10 16:39:01 +0200

    height is dynamic now

--- Sources/Utility Toolbox/Extensions/SwiftUI/View.swift
+++ Sources/Utility Toolbox/Extensions/SwiftUI/View.swift
@@ -76,7 +76,6 @@ extension View {
         cornerRadius: CGFloat = 8,
         textColor: Color = .primary,
         backgroundColor: Color = .gray5,
-        height: CGFloat = CGSize.screen.height * 0.05,
         focusField: FocusState<Field?>.Binding,
         focusAction: (() -> Void)? = nil,
         cancelAction: (() -> Void)? = nil) -> some View {
@@ -84,7 +83,6 @@ extension View {
                                            cornerRadius: cornerRadius,
                                            textColor: textColor,
                                            backgroundColor: backgroundColor,
-                                           height: height,
                                            focusField: focusField,
                                            focusAction: focusAction,
                                            cancelAction: cancelAction)
