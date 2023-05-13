e38bbee9f254c61aea5be7996743ca367bd16c16
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-13 23:37:38 +0200

    Added animation completion

--- Sources/Utility Toolbox/Extensions/SwiftUI/View.swift
+++ Sources/Utility Toolbox/Extensions/SwiftUI/View.swift
@@ -11,15 +11,26 @@ import SwiftUI
 
 extension View {
     /// Returns a snapshot of a view as an image.
-    @MainActor public func image() -> UIImage? {
+    @MainActor public func image(isOpaque: Bool = false) -> UIImage? {
         if #available(iOS 16, *) {
             let renderer = ImageRenderer(content: self)
             renderer.scale = UIScreen.main.scale
-            renderer.isOpaque = true
+            renderer.isOpaque = isOpaque
             let renderedImage = renderer.uiImage
             return renderedImage
         }
         return nil
+    }
+    
+    /// Calls the completion handler whenever an animation on the given value completes.
+    /// - Parameters:
+    ///   - value: The value to observe for animations.
+    ///   - completion: The completion callback to call once the animation completes.
+    /// - Returns: A modified `View` instance with the observer attached.
+    public typealias AnimationCompletion<Value: VectorArithmetic> = ModifiedContent<Self, AnimationCompletionObserverModifier<Value>>
+    public func onAnimationCompleted<Value: VectorArithmetic>(for value: Value,
+                                                       completion: @escaping () -> Void) -> AnimationCompletion<Value> {
+        modifier(AnimationCompletionObserverModifier(observedValue: value, completion: completion))
     }
 }
 
---
+++ Sources/Utility Toolbox/Models/SwiftUI/Modifiers/View Modifiers/AnimationCompletionObserverModifier.swift
@@ -0,0 +1,48 @@
+//
+//  AnimationCompletionObserverModifier.swift
+//  
+//
+//  Created by Maertens Yann-Christophe on 13/05/23.
+//
+
+import SwiftUI
+
+/// An animatable modifier that is used for observing animations for a given animatable value.
+public struct AnimationCompletionObserverModifier<Value>: AnimatableModifier where Value: VectorArithmetic {
+    
+    /// While animating, SwiftUI changes the old input value to the new target value using this property. This value is set to the old value until the animation completes.
+    public var animatableData: Value {
+        didSet {
+            notifyCompletionIfFinished()
+        }
+    }
+    
+    /// The target value for which we're observing. This value is directly set once the animation starts. During animation, `animatableData` will hold the oldValue and is only updated to the target value once the animation completes.
+    private var targetValue: Value
+    
+    /// The completion callback which is called once the animation completes.
+    private var completion: () -> Void
+    
+    public init(observedValue: Value,
+                completion: @escaping () -> Void) {
+        self.completion = completion
+        self.animatableData = observedValue
+        targetValue = observedValue
+    }
+    
+    /// Verifies whether the current animation is finished and calls the completion callback if true.
+    private func notifyCompletionIfFinished() {
+        guard animatableData == targetValue else { return }
+        
+        /// Dispatching is needed to take the next runloop for the completion callback.
+        /// This prevents errors like "Modifying state during view update, this will cause undefined behavior."
+        DispatchQueue.main.async {
+            self.completion()
+        }
+    }
+    
+    public func body(content: Content) -> some View {
+        /// We're not really modifying the view so we can directly return the original input value.
+        return content
+    }
+}
