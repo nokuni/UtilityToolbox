6b4b0f01699d35dbfb0389d473004f5fb784a7fc
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-14 21:21:45 +0200

    Added more gradient

--- Sources/Utility Toolbox/Extensions/SwiftUI/Gradient.swift
+++ Sources/Utility Toolbox/Extensions/SwiftUI/Gradient.swift
@@ -11,4 +11,6 @@ public extension Gradient {
     static let rainbow = Gradient(colors: [.red, .orange, .yellow, .green, .blue, .indigo, .purple])
     static let summerBreeze = Gradient(colors: [Color(hex: 0xf9c58d), Color(hex: 0xf492f0)])
     static let blueSea = Gradient(colors: [Color(hex: 0x0061ff), Color(hex: 0x60efff)])
+    static let sunset = Gradient(colors: [Color(hex: 0xffcf67), Color(hex: 0xd3321d)])
+    static let clearSea = Gradient(colors: [Color(hex: 0x6ef195), Color(hex: 0x00e3fd)])
 }
