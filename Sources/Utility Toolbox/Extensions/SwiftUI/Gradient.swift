b3330c697266d485ddb04f14a267be8235e1aeaf
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-15 23:59:00 +0200

    Added diamond gradient

--- Sources/Utility Toolbox/Extensions/SwiftUI/Gradient.swift
+++ Sources/Utility Toolbox/Extensions/SwiftUI/Gradient.swift
@@ -12,6 +12,9 @@ public extension Gradient {
     static let lightRainbow = Gradient(colors: [
         Color(hex: 0xffb6c1), Color(hex: 0xe0b0ff), Color(hex: 0x7df9ff), Color(hex: 0xdffff8f), Color(hex: 0xffbf00)
     ])
+    static let diamond = Gradient(colors: [
+        Color(hex: 0xf1f7fb), Color(hex: 0xd9ebf4), Color(hex: 0xcbe3f0), Color(hex: 0xb8d8e7), Color(hex: 0x9ac5db)
+    ])
     static let summerBreeze = Gradient(colors: [Color(hex: 0xf9c58d), Color(hex: 0xf492f0)])
     static let blueSea = Gradient(colors: [Color(hex: 0x0061ff), Color(hex: 0x60efff)])
     static let sunset = Gradient(colors: [Color(hex: 0xffcf67), Color(hex: 0xd3321d)])
