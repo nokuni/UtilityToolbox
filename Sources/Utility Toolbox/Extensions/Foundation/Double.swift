6e21ffd9dd02c55f53a5e303ef873df9ce823f41
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-05-25 15:15:44 +0200

    Added withouttrailingzeros method

--- Sources/Utility Toolbox/Extensions/Foundation/Double.swift
+++ Sources/Utility Toolbox/Extensions/Foundation/Double.swift
@@ -11,4 +11,12 @@ public extension Double {
     func percentageValue(percentage: Double) -> Double {
         return (percentage * self) / 100
     }
+    
+    func withoutTrailingZeros() -> String {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        let number: NSNumber = NSNumber(floatLiteral: self)
+        guard let result = formatter.string(from: number) else { return "" }
+        return result
+    }
 }
