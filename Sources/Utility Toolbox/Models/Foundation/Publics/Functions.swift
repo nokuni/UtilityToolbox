e48a91d92f535e9003cb160caa8851a2aeb4165d
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-06-01 17:32:06 +0200

    Added methods

--- Sources/Utility Toolbox/Models/Foundation/Publics/Functions.swift
+++ Sources/Utility Toolbox/Models/Foundation/Publics/Functions.swift
@@ -5,7 +5,7 @@
 //  Created by Maertens Yann-Christophe on 23/03/23.
 //
 
-import Foundation
+import SwiftUI
 
 /// Repeat an action following a condtion. The amount of repeated actions is limited to 10000 by default.
 public func repeatWhile(limit: Int, repeatedCompletion: (() -> Void)? = nil, endCompletion: (() -> Void)? = nil) {
@@ -27,4 +27,23 @@ public func inData<M: Codable>(value: M) throws -> Data? {
     let object = try encode(value)
     let data = try? JSONSerialization.data(withJSONObject: object)
     return data
+}
+
+/// Returns a dictionary of arguments from constructs.
+public func reflected<S>(value: S) -> [String: Any] {
+    let mirror = Mirror(reflecting: value)
+    let labels = mirror.children.compactMap { $0.label }
+    let values = mirror.children.compactMap { $0.value }
+    let result = Dictionary(uniqueKeysWithValues: zip(labels, values))
+    return result
+}
+
+public func isImageInAssets(image: String) -> Bool {
+    let uiImage = UIImage(named: image)
+    return uiImage != nil
+}
+
+public func isImagesInAssets(images: [String]) -> Bool {
+    let uiImages = images.compactMap { UIImage(named: $0) }
+    return uiImages.count == images.count
 }
