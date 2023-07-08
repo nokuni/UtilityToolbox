7ad1fbd87472934538479f5862b1f6319e217310
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-07-08 17:18:10 +0200

    update and improve center cropped method

--- Sources/Utility Toolbox/Extensions/SwiftUI/Image/Image.swift
+++ Sources/Utility Toolbox/Extensions/SwiftUI/Image/Image.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 public extension Image {
-
+    
     /// Zoomed center of an image
-    func centerCropped(radius: CGFloat) -> some View {
+    func centerCropped(cornerRadius: CGFloat = 0,
+                       zoomedPart: Alignment = .center) -> some View {
         GeometryReader { geo in
             self
                 .resizable()
                 .scaledToFill()
-                .frame(width: geo.size.width, height: geo.size.height)
-                .clipShape(RoundedRectangle(cornerRadius: radius))
+                .frame(width: geo.size.width, height: geo.size.height, alignment: zoomedPart)
+                .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
         }
     }
-
+    
     /// Get Image from data.
     static func data(_ data: Data) -> Image? {
         if let uiimage = UIImage(data: data) {
@@ -34,59 +35,59 @@ public extension Image {
     init(sfGaming: SFGamingSymbol) {
         self.init(systemName: sfGaming.rawValue)
     }
-
+    
     init(sfWeather: SFWeatherSymbol) {
         self.init(systemName: sfWeather.rawValue)
     }
-
+    
     init(sfAccessibility: SFAccessibilitySymbol) {
         self.init(systemName: sfAccessibility.rawValue)
     }
-
+    
     init(sfTime: SFTimeSymbol) {
         self.init(systemName: sfTime.rawValue)
     }
-
+    
     init(sfPrivacyAndSecurity: SFPrivacyAndSecuritySymbol) {
         self.init(systemName: sfPrivacyAndSecurity.rawValue)
     }
-
+    
     init(sfDevices: SFDevicesSymbol) {
         self.init(systemName: sfDevices.rawValue)
     }
-
+    
     init(sfShapes: SFShapesSymbol) {
         self.init(systemName: sfShapes.rawValue)
     }
-
+    
     init(sfArrow: SFArrowsSymbol) {
         self.init(systemName: sfArrow.rawValue)
     }
-
+    
     init(sfMaths: SFMathsSymbol) {
         self.init(systemName: sfMaths.rawValue)
     }
-
+    
     init(sfTransport: SFTransportSymbol) {
         self.init(systemName: sfTransport.rawValue)
     }
-
+    
     init(sfTextFormatting: SFTextFormattingSymbol) {
         self.init(systemName: sfTextFormatting.rawValue)
     }
-
+    
     init(sfMedia: SFMediaSymbol) {
         self.init(systemName: sfMedia.rawValue)
     }
-
+    
     init(sfHealth: SFHealthSymbol) {
         self.init(systemName: sfHealth.rawValue)
     }
-
+    
     init(sfNature: SFNatureSymbol) {
         self.init(systemName: sfNature.rawValue)
     }
-
+    
     init(sfVariable: SFVariableSymbol) {
         self.init(systemName: sfVariable.rawValue)
     }
