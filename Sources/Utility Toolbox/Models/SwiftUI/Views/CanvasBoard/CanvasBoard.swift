528baa2cc77b6c2d412b4ebdd6ae9905d318c206
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2024-02-20 21:37:36 +0100

    fix: canva view

--- Sources/Utility Toolbox/Models/SwiftUI/Views/CanvasBoard/CanvasBoard.swift
+++ Sources/Utility Toolbox/Models/SwiftUI/Views/CanvasBoard/CanvasBoard.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 
 public struct CanvasBoard: View {
-    public init(manager: CanvasManager) {
-        self.manager = manager
-    }
     @ObservedObject var manager: CanvasManager
     public var body: some View {
         manager.canvasView
