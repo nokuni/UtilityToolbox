07701c063a86a54f58cde74f11a61966bee0dde5
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2024-02-22 13:36:09 +0100

    fix: image highlight fix

--- Sources/UtilityToolbox/Models/SwiftUI/Modifiers/View Modifiers/ImageHighlight.swift
+++ Sources/UtilityToolbox/Models/SwiftUI/Modifiers/View Modifiers/ImageHighlight.swift
@@ -12,11 +12,14 @@ public struct ImageHighlightModifier: ImageModifier {
     public var alpha: Double
     public var interpolation: Image.Interpolation
     public func body(image: Image) -> some View {
-        image
-            .interpolation(interpolation)
-            .renderingMode(.template)
-            .resizable()
-            .foregroundColor(color)
-            .opacity(alpha)
+        ZStack {
+            image
+            image
+                .interpolation(interpolation)
+                .renderingMode(.template)
+                .resizable()
+                .foregroundColor(color)
+                .opacity(alpha)
+        }
     }
 }
