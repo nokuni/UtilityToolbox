c340ff4fd5451f54fa240a52fcb6ce5bef74bed8
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2024-04-09 09:45:40 +0200

    feat: added isNil

--- Sources/UtilityToolbox/Extensions/Foundation/Optional.swift
+++ Sources/UtilityToolbox/Extensions/Foundation/Optional.swift
@@ -1,8 +1,38 @@
 //
-//  File.swift
+//  Optional.swift
 //  
 //
 //  Created by Yann Christophe Maertens on 09/04/2024.
 //
 
 import Foundation
+
+extension Optional where Wrapped == String {
+    var isNil: Bool {
+        return self == nil
+    }
+}
+
+extension Optional where Wrapped == Int {
+    var isNil: Bool {
+        return self == nil
+    }
+}
+
+extension Optional where Wrapped == Double {
+    var isNil: Bool {
+        return self == nil
+    }
+}
+
+extension Optional where Wrapped == Date {
+    var isNil: Bool {
+        return self == nil
+    }
+}
+
+extension Optional where Wrapped: Collection {
+    var isNil: Bool {
+        return self == nil
+    }
+}
